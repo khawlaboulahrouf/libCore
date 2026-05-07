1a921cbdf1c721f1a6edfab1c2dddb154b520f4b
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-05-07 12:39:54 +0100

    ajouter constructeur class user

--- src/Entities/user.php
+++ src/Entities/user.php
@@ -6,5 +6,12 @@ abstract class User
     private string $name;
     private string $email;
 
-    
+    // ── Constructeur ─────────────────────────────────────────
+    public function __construct(int $id, string $name, string $email)
+    {
+        $this->id    = $id;
+        $this->name  = $name;
+        $this->email = $email;
+    }
+
 }
